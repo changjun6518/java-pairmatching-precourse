14c4b236737e8d0852102478d9c020d6cf51d84e
최창준 <aowert6518@gmail.com>
2021-12-23 21:28:33 +0900

    [feat] - 사용자에게 1,2,3,Q를 입력받는다

---
+++ src/main/java/pairmatching/constant/InputViewMessage.java
@@ -0,0 +1,11 @@
+package pairmatching.constant;
+
+public enum InputViewMessage {
+    ASK_USER_CHOICE_MESSAGE("기능을 선택하세요.");
+
+    private String message;
+
+    InputViewMessage(String message) {
+        this.message = message;
+    }
+}
