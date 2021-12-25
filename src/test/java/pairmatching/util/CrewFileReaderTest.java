cb2f4bc771a4b12b9e7081c6a1799115e90f6bad
최창준 <aowert6518@gmail.com>
2021-12-25 17:39:44 +0900

    [test] - 매칭 결과 테스트

--- src/test/java/pairmatching/util/CrewFileReaderTest.java
+++ src/test/java/pairmatching/util/CrewFileReaderTest.java
@@ -7,13 +7,14 @@ import pairmatching.domain.Matching;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static pairmatching.constant.SystemMessage.BACK_END_FILE_PATH;
 
 class CrewFileReaderTest {
     @Test
     public void read_file_test() throws Exception{
         // given
         // when
-        List<String> crewList = CrewFileReader.readCrewList();
+        List<String> crewList = CrewFileReader.readCrewList(BACK_END_FILE_PATH);
         // then
         Assertions.assertThat(crewList.contains("백호")).isEqualTo(true);
         Assertions.assertThat(crewList.contains("태웅")).isEqualTo(true);
--- src/test/java/pairmatching/view/OutputViewTest.java
+++ src/test/java/pairmatching/view/OutputViewTest.java
@@ -2,14 +2,25 @@ package pairmatching.view;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import pairmatching.util.CrewFileReader;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static pairmatching.constant.SystemMessage.BACK_END_FILE_PATH;
+import static pairmatching.constant.SystemMessage.FRONT_END_FILE_PATH;
 
 class OutputViewTest {
     @Test
     public void printProcessLevelMission_Test() throws Exception{
         // then
         OutputView.printProcessLevelMission();
+    }
+
+    @Test
+    public void print_matching_result() throws Exception {
+        // given
+        // when
+        // then
+        OutputView.printMatchingResult(CrewFileReader.readCrewList(FRONT_END_FILE_PATH));
     }
 
 }
