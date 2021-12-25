9470ed6a5e360bf6a7156b2094a975718470f2f4
최창준 <aowert6518@gmail.com>
2021-12-25 16:05:01 +0900

    [refactor] Level안에 미션들 초기화
    - 미션초기화하는 과정을 Enum인 Level안에서 바로 처리함

--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -1,19 +1,18 @@
 package pairmatching.view;
 
 import pairmatching.domain.Course;
-import pairmatching.domain.MissionFactory;
+import pairmatching.domain.Level;
 
 public class OutputView {
     public static void printProcessLevelMission() {
-        MissionFactory.init();
         System.out.println("#############################################\n" +
                             Course.print() + "\n" +
                             "미션:\n" +
-                            MissionFactory.level1.printMissions() + "\n" +
-                            MissionFactory.level2.printMissions() + "\n" +
-                            MissionFactory.level3.printMissions() + "\n" +
-                            MissionFactory.level4.printMissions() + "\n" +
-                            MissionFactory.level5.printMissions() + "\n" +
+                            Level.LEVEL1.printMissions() + "\n" +
+                            Level.LEVEL2.printMissions() + "\n" +
+                            Level.LEVEL3.printMissions() + "\n" +
+                            Level.LEVEL4.printMissions() + "\n" +
+                            Level.LEVEL5.printMissions() + "\n" +
                             "############################################");
     }
 }
--- src/test/java/pairmatching/LevelTest.java
+++ src/test/java/pairmatching/LevelTest.java
@@ -11,17 +11,9 @@ public class LevelTest {
     @Test
     public void to_string_test() throws Exception {
         // given
-        Level level = Level.LEVEL1;
-
         // when
-        List<String> missionList = new ArrayList<>();
-        missionList.add("자동차경주");
-        missionList.add("로또");
-        missionList.add("숫자야구게임");
-        level.setMissions(missionList);
-
         // then
-        Assertions.assertThat("레벨1: 자동차경주 | 로또 | 숫자야구게임").isEqualTo(level.printMissions());
-        System.out.println(level.printMissions());
+        Assertions.assertThat("  - 레벨1: 자동차경주 | 로또 | 숫자야구게임").isEqualTo(Level.LEVEL1.printMissions());
+        System.out.println(Level.LEVEL1.printMissions());
     }
 }
