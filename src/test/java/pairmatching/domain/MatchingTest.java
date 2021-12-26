56f6674b0e4fbd0b2294351c1198d323982ad8da
최창준 <aowert6518@gmail.com>
2021-12-26 17:29:18 +0900

    [test] - 매칭 후 각 크루원의 파트너가 만난 리스트에 잘 저장되는지 확인

--- src/test/java/pairmatching/domain/MatchingTest.java
+++ src/test/java/pairmatching/domain/MatchingTest.java
@@ -2,6 +2,7 @@ package pairmatching.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,6 +18,25 @@ class MatchingTest {
         // then
         for (String s : shuffleList) {
             System.out.println(s);
+        }
+    }
+
+    @Test
+    public void after_match_test() throws Exception {
+        // given
+        Matching matching = new Matching();
+        // when
+        matching.shuffleList();
+        matching.updateMatchedCrew(Level.LEVEL1);
+        // then
+        CrewRepository crewRepository = matching.getCrewRepository();
+        List<Crew> backEndCrewList = crewRepository.getBackEndCrewList();
+        Crew crew = backEndCrewList.get(1);
+        System.out.println(crew.getName());
+        HashMap<Level, List<Crew>> matchedCrew = crew.getMatchedCrew();
+        List<Crew> crews = matchedCrew.get(Level.LEVEL1);
+        for (Crew crew1 : crews) {
+            System.out.println(crew1.getName());
         }
     }
 }
