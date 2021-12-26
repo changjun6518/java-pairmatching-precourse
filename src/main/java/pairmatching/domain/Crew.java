0419dcf9543d3392a33695d8c336753bea9f4498
최창준 <aowert6518@gmail.com>
2021-12-26 16:19:00 +0900

    [feat] - 각 과정마다 크루원들을 따로 초기화 시킨다

--- src/main/java/pairmatching/domain/Crew.java
+++ src/main/java/pairmatching/domain/Crew.java
@@ -1,11 +1,27 @@
 package pairmatching.domain;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Crew {
     private String name;
-    private Course course;
+    private HashMap<Level, List<Crew>> matchedCrew = new HashMap<>();
 
     public Crew(String name) {
         this.name = name;
+        init();
     }
+
+    private void init() {
+        for (Level level : Level.values()) {
+            matchedCrew.put(level, new ArrayList<>());
+        }
+    }
+
+    public boolean isPossibleMatch(Level level, Crew crew) {
+        return !matchedCrew.get(level).contains(crew);
+    }
+
 
 }
--- src/main/java/pairmatching/domain/CrewRepository.java
+++ src/main/java/pairmatching/domain/CrewRepository.java
@@ -8,15 +8,20 @@ import java.util.stream.Collectors;
 import static pairmatching.constant.SystemMessage.BACK_END_FILE_PATH;
 
 public class CrewRepository {
-    private List<Crew> crewList;
+    private List<Crew> backEndCrewList;
+    private List<Crew> frontEndCrewList;
 
     public CrewRepository() {
         initCrewList();
     }
 
     private void initCrewList() {
-        List<String> stringList = CrewFileReader.readCrewList(BACK_END_FILE_PATH);
-        crewList = stringList.stream()
+        List<String> backStringList = CrewFileReader.readCrewList(BACK_END_FILE_PATH);
+        List<String> frontStringList = CrewFileReader.readCrewList(BACK_END_FILE_PATH);
+        backEndCrewList = backStringList.stream()
+                .map(Crew::new)
+                .collect(Collectors.toList());
+        frontEndCrewList = frontStringList.stream()
                 .map(Crew::new)
                 .collect(Collectors.toList());
     }
